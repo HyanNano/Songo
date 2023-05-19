b78015e97aa2abe94aff48d36053d2ba745cd763
nano <kengnihyan5@gmail.com>
2023-05-20 00:35:54 +0100

    Good !

--- js/songo.js
+++ js/songo.js
@@ -23,8 +23,34 @@ class Songo{
     }
 
     estBloque(idJ) {
-        //qui retourne 0 ou 1 si le joueur est respectivement bloque ou non
+        //qui retourne 1 ou 0 si le joueur est respectivement bloque ou non
 
+        if(idJ == 1){
+            let somme1 = 0;
+            for (let index = 0; index < this.coteJoueur1.length; index++) {
+                const element = this.coteJoueur1[index];
+                
+                somme1 += element;
+            }
+            if((somme1 == 0) || (somme1 == this.coteJoueur1[6] && somme1 > 2)){
+                //Le joueur est bloque
+                return 1;
+            }
+            return 0;
+        }
+        if(idJ == 2){
+            let somme2 = 0;
+            for (let index = 0; index < this.coteJoueur2.length; index++) {
+                const element = this.coteJoueur2[index];
+                
+                somme2 += element;
+            }
+            if((somme2 == 0) || (somme2 == this.coteJoueur2[6] && somme2 > 2)){
+                //Le joueur est bloque
+                return 1;
+            }
+            return 0;
+        }
     }
 
     distribution(idJ,indice_cellule) {
@@ -35,47 +61,72 @@ class Songo{
         let indice_cellule_prise = 0;
         let tableau = this.tableau_distribution();
 
-        //alert(indice_cellule);
-
         let valeur;
-
         valeur = tableau[indice_cellule];
-        tableau[indice_cellule] = 0;
-
-
 
         //cellule ici est la cellule sur laquelle on a clique
 
+        let jeu = true;
 
-        if(valeur == 0){
-            //Le tableau ne change pas.
+        if(indice_cellule == 6 || indice_cellule ==13){
+            
+            jeu = false;
 
-            //Le joueur doit rejouer
-
-        }else{
-
-            if(indice_cellule == 6 || indice_cellule ==13){
-
-                if(valeur == 1 || valeur == 2){
-                    //gestion des interdits
-
+            if(valeur == 1 || valeur == 2){
+                //gestion des interdits
+                if(valeur == 2){
                     //si il y aura prise, on peut jouer la case
+                    let indice_test_prise = indice_cellule + 1 + valeur - 1;//indice_cellule_prise
+                    let test_valeur = tableau[(indice_test_prise)%14] + 1;//valeur de la cellule_prise apres la distribution
+                    if(2 <= test_valeur && test_valeur <= 4){
+                        //Donc il y aura prise, on joue.
+                        jeu = true;
+                    } 
 
                     //si c'est la seule case qui dispose des pierres,on peut la jouer
-
-                }
-            }else{
-            
-                for(let i = indice_cellule+1; i< indice_cellule+1+valeur;i++){
-                    //ici on a i comme l'indice des prochaines cellules a incrementer.
-                    let index = i%14;
-                    
-                    tableau[index]++;
-                    //La cellule est incrementer
-                
-                    indice_cellule_prise = index;
+                    if(idJ == 1){
+                        let somme1 = 0;
+                        for (let index = 0; index < this.coteJoueur1.length; index++) {
+                            const element = this.coteJoueur1[index];
+                            
+                            somme1 += element;
+                        }
+                        if(somme1 == 2){
+                            // Donc c'est la seule case du joueur qui dispose de pierres.
+                            jeu = true;
+                        }
+                    }
+                    if(idJ == 2){
+                        let somme2 = 0;
+                        for (let index = 0; index < this.coteJoueur2.length; index++) {
+                            const element = this.coteJoueur2[index];
+                            
+                            somme2 += element;
+                        }
+                        if(somme2 == 2){
+                            // Donc c'est la seule case du joueur qui dispose de pierres.
+                            jeu = true;
+                        }
+                    }
                 }
             }
+        }
+
+        if(jeu){
+            // Jeu
+            // incrementation
+            for(let i = indice_cellule+1; i< indice_cellule+1+valeur;i++){
+                //ici on a i comme l'indice des prochaines cellules a incrementer.
+                let index = i%14;
+                
+                tableau[index]++;
+                //La cellule est incrementer
+            
+                indice_cellule_prise = index;
+            }
+            // mise a zero
+            tableau[indice_cellule] = 0;
+
         }
         
 
@@ -92,43 +143,79 @@ class Songo{
 
         //cellule ici est la cellule sur laquelle on a clique
 
-        // alert("test");
-
-        let indice_cellule_prise = this.distribution(idJ,indice_cellule) + 1;
-        // alert(indice_cellule);
+        let indice_cellule_prise = this.distribution(idJ,indice_cellule);
 
         let tableau = this.tableau_distribution();
 
-        let cellule_prise = tableau[indice_cellule_prise];
-        //cellule_prise est la cellule sur laquelle on testera s'il y a une prise ou non.
+        // si au terme d'une distribution, toutes les 7 cases d'un joueur ont un nombre compris entre 2 et 4, aucune prise n'est faite.
+        let prise = false;
+        for (let index = 0; index < this.coteJoueur1.length; index++) {
+            const element = this.coteJoueur1[index];
 
-        // alert(cellule_prise);
-        let cellule_prise_suivante = tableau[indice_cellule_prise + 1];
-        //cellule_prise_suivante
-
-        if(2 <= cellule_prise && cellule_prise <= 4){
-            //si la cellule prise est entre 2 et 4.
-
-            if(idJ == 1){
-                this.pointJoueur1 += cellule_prise;
+            if((2 <= element && element <= 4 ) && (!prise) ){
+                //on passe a l'element suivant
             }else{
-                this.pointJoueur2 += cellule_prise;
+                //on a un element qui n'est pas compris entre 2 et 4
+                prise = true;
             }
-            tableau[indice_cellule_prise] = 0;
-
-
-            if(2 <= cellule_prise_suivante && cellule_prise_suivante <= 4){
-                //si la cellule prise est entre 2 et 4.
-
-                if(idJ == 1){
-                    this.pointJoueur1 += cellule_prise_suivante;
+            
+        }
+        if(!prise){
+            //si prise est toujours false i.e tous les elements du joueur 1 son compris entre 2 et 4 alors on teste ceux du joueur 2.
+            for (let index = 0; index < this.coteJoueur2.length; index++) {
+                const element = this.coteJoueur2[index];
+    
+                if((2 <= element && element <= 4 ) && (!prise) ){
+                    //on passe a l'element suivant
                 }else{
-                    this.pointJoueur2 += cellule_prise_suivante;
+                    //on a un element qui n'est pas compris entre 2 et 4
+                    prise = true;
                 }
                 
-                tableau[indice_cellule_prise + 1] = 0;
+            }
+        }
+
+        if(prise){
+            // si la distribution se termine sur une cellule adverse
+            let cellule_adverse = false;
+            if(indice_cellule_prise < 7 && idJ == 2){
+                cellule_adverse = true;
+            }
+            if(indice_cellule_prise >= 7 && idJ == 1){
+                cellule_adverse = true;
             }
 
+            if(cellule_adverse){
+                let cellule_prise = tableau[indice_cellule_prise];
+                //cellule_prise est la cellule sur laquelle on testera s'il y a une prise ou non.
+
+                let cellule_prise_precedente = tableau[(indice_cellule_prise - 1)%14];
+                //cellule_prise_precedente
+
+                if(2 <= cellule_prise && cellule_prise <= 4){
+                    //si la cellule prise est entre 2 et 4.
+
+                    if(idJ == 1){
+                        this.pointJoueur1 += cellule_prise;
+                    }else{
+                        this.pointJoueur2 += cellule_prise;
+                    }
+                    tableau[indice_cellule_prise] = 0;
+
+
+                    if(2 <= cellule_prise_precedente && cellule_prise_precedente <= 4){
+                        //si la cellule prise est entre 2 et 4.
+
+                        if(idJ == 1){
+                            this.pointJoueur1 += cellule_prise_precedente;
+                        }else{
+                            this.pointJoueur2 += cellule_prise_precedente;
+                        }
+                        
+                        tableau[(indice_cellule_prise - 1)%14] = 0;
+                    }
+                }
+            }
         }
 
         this.actualiser(tableau);
@@ -246,43 +333,100 @@ class Songo{
         document.getElementById("player2[5]").innerHTML = this.coteJoueur2[5];
         document.getElementById("player2[6]").innerHTML = this.coteJoueur2[6];
         
-        // alert("OOAaa");
         document.getElementById("pointJoueur1").innerHTML = this.pointJoueur1;
-        // alert("qwertz");
         document.getElementById("pointJoueur2").innerHTML = this.pointJoueur2;
     }
     
 }
 
+// const controller = new AbortController();
 
-monSongo = new Songo();
-
-function main(event){
-    monSongo.afficher();
-    //alert("oops!");
+function abonne(event){
     let indice = parseInt(event.target.value);
-    //let indice = 5; 
-    //alert(indice);
     let idJ;
     if(indice < 7){
         idJ = 1;
-        //alert(idJ);
     }
     else{
         idJ = 2;        
-        // alert(idJ);
     };
 
-    //alert("uuuu");
     monSongo.prise(idJ, indice);
-    // alert("kkkk");
-    // monSongo.afficher();
-    // alert("fffin");
+
+    // Poursuite du Jeu
+    if(!(monSongo.poursuiteJeu())){
+        //Joueur bloque ?
+        if(monSongo.estBloque(idJ)){
+            alert("Le joueur " + idJ + "est bloque !");
+            //Fin du jeu
+            alert("Match Nul !");
+            // controller.abort();
+        }
+    }else{
+        //Fin du jeu.
+        alert("Le joueur " + monSongo.poursuiteJeu() + " a gagne.");
+        // controller.abort();
+    }
 }
 
+function machine(event){
+    let indice = parseInt(event.target.value);
+    let idJ;
+    if(indice < 7){
+        idJ = 1;
+    
 
-const buttons = document.querySelectorAll("th > button");
+        monSongo.prise(idJ, indice);
+    
+        let idM = 2 ;
+        let indice_machine = Math.floor(Math.random() * 6) + 7;
+        setTimeout(() => {
+            monSongo.prise(idM,indice_machine)
+        }, 3000);
+    }   
 
-for (const button of buttons) {
-  button.addEventListener("click", main);
+    // Poursuite du Jeu
+    if(!(monSongo.poursuiteJeu())){
+        //Joueur bloque ?
+        if(monSongo.estBloque(idJ)){
+            alert("Le joueur " + idJ + " est bloque !");
+            //Fin du jeu
+            alert("Match Nul !");
+            // controller.abort();
+        }
+    }else{
+        //Fin du jeu.
+        alert("Le joueur " + monSongo.poursuiteJeu() + " a gagne.");
+        // controller.abort();
+    }
 }
+
+function jouer_vs_abonne(){
+    monSongo = new Songo();
+    monSongo.afficher();
+
+    const buttons = document.querySelectorAll("th > button");
+
+    for (const button of buttons) {
+    //button.addEventListener("click", abonne, { signal: controller.signal });
+    button.onclick = abonne;
+    }
+}
+
+function jouer_vs_machine(){
+    monSongo = new Songo();
+    monSongo.afficher();
+
+    const buttons = document.querySelectorAll("th > button");
+
+    for (const button of buttons) {
+    //button.addEventListener("click", machine, { signal: controller.signal});
+    button.onclick = machine;
+    }
+}
+
+const vs_machine = document.querySelector("#vs_machine");
+const vs_abonne = document.querySelector("#vs_abonne");
+
+vs_abonne.addEventListener("click", jouer_vs_abonne);
+vs_machine.addEventListener("click", jouer_vs_machine);
